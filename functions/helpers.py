8ab5963ddd6c6a07e5bf232480f3d81526a4b762
stepyt <stepyt.crypto@gmail.com>
2022-06-20 11:50:21 +0200

    Added histogram no bg plots

--- functions/helpers.py
+++ functions/helpers.py
@@ -92,6 +92,35 @@ def make_histogram (data: np.array, bins: int, title: str, boundaries: int = Non
     if save_fig:
         plt.savefig(f'assets/images/{fig_title}', format='png', dpi=600)
 
+def make_histogram_no_background (data: np.array, bins: int, title: str, boundaries: int = None, save_fig: bool = False, fig_title: str = ''):
+
+    colors = iter(make_color_line(bins))
+
+    plt.figure(figsize=(7,7), facecolor=black_deep)
+    ax = plt.axes()
+    ax.tick_params(axis='x', colors=gray)
+    ax.xaxis.label.set_color(gray)
+    ax.yaxis.label.set_color(gray)
+    ax.spines['bottom'].set_color(gray)
+    ax.spines['left'].set_color(gray)
+    ax.spines['top'].set_color(gray)
+    ax.spines['right'].set_color(gray)
+    ax.title.set_color(gray)
+    ax.tick_params(axis='y', colors=gray)
+    plt.annotate(
+        r'@stepyt', xy=(0.5, 0.5), alpha=0.5,
+        ha='left', va='bottom', color=gray, size=11
+    )
+    _, _, patches = ax.hist(data, bins, edgecolor='white', linewidth=1)
+    plt.title(f'{title.upper()}', fontweight='bold', fontsize='16', pad=20)
+    plt.xlabel("Airdrop amount", fontweight='bold', fontsize='13')
+    plt.ylabel("Number of addresses", fontweight='bold', fontsize='13')
+    ax.patch.set_alpha(0.)
+    for i in range(0,bins):
+        patches[i].set_facecolor(next(colors))
+    if save_fig:
+        plt.savefig(f'assets/images/{fig_title}', transparent=True, format='png', dpi=600)
+
 def convert_to_micro(amount: float) -> float:
     """Convert input to micro unit: juno -> ujuno"""
     return amount * 1e6
